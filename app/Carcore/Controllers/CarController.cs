3b888bd03b4f65d0fdfc4a3860f248fc4646a64f
= <felix.ganterer@stud.th-rosenheim.de>
2023-04-17 19:38:20 +0200

    add GetAllMakes

--- app/Carcore/Controllers/CarController.cs
+++ app/Carcore/Controllers/CarController.cs
@@ -17,6 +17,13 @@ namespace Carcore.Controllers
         private const string connectionString = "mongodb://127.0.0.1:27017";
         private const string databaseName = "carcore_db";
         private const string makeCollection = "makes";
+        private readonly IConfiguration _config;
+
+        public CarController(IConfiguration config)
+        {
+            _config = config;
+        }
+
 
         public IMongoCollection<T> ConnectToMongo<T>(in string collection)
         {
@@ -25,6 +32,33 @@ namespace Carcore.Controllers
             return db.GetCollection<T>(collection);
         }
 
+
+        [Route("getAllMakes")]
+        [HttpGet]
+        public async Task<List<CarModel>> GetAllMakes()
+        { 
+            string url = "https://vpic.nhtsa.dot.gov/api/vehicles/GetAllMakes?format=json";
+            List<string> allowedMakes = _config.GetSection("AllowedMakes").Get<List<string>>();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(url);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    CarResultModel result = JsonConvert.DeserializeObject<CarResultModel>(responseContent);
+                    List<CarModel> makes = result.Results.Where(m => allowedMakes.Contains(m.Make_Name)).ToList();
+                    return makes;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
 
         // todo: Nach welcher Marke sucht der client?
         [Route("GetModelsForMake")]
