9713dfaf733eacc9fe86ae05183f64658ca2db98
= <felix.ganterer@stud.th-rosenheim.de>
2023-06-22 23:45:33 +0200

    Projektdatei tauschen für docker-natives projekt

---
+++ app/Carcore/Carcore/Controllers/DemoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Carcore.Controllers
+{
+    [ApiController]
+    [Route("demo")]
+    public class DemoController : Controller
+    {
+        [HttpGet]
+        public String Get()
+        {
+            return "Welcome to the Docker World!";
+        }
+    }
+}
